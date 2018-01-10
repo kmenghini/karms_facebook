a77fa52d86f720d948e5da3c3b433e3030c061d0
Albert Chang <albertchanged@gmail.com>
2018-01-09 23:59:07 -0800

    Import axios, implement CreatePost, Post-related server routes
    All team members should do 'npm install' again after pulling.
    Created server routes for getting all posts and making a new post.
    Refactored Post and PostList to render input text

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import Post from './Post.jsx';
 import CreatePost from './CreatePost.jsx';
+import axios from 'axios';
 
 class PostList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      receivedText: ''
+    }
+  }
+  componentDidMount() {
+    this.getNewPosts();
+  }
+  getNewPosts() {
+    let username = 'albertchanged';
+    axios.get(`/${username}/posts`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  receivePostText(text) {
+    console.log(text);
+    this.setState({
+      receivedText: text
+    })
   }
   render() {
     return (
       <div>
-        <CreatePost />
+        <CreatePost onClick={this.receivePostText.bind(this)} />
         <br />
         {
+          // this.props.postList.map((post) => (
+          //   <Post
+          //     postText={this.state.receivedText}
+          //   />
+          // ))
           <div>
-          <Post />
+          <Post postText={this.state.receivedText}/>
           <br />
           <Post />
           </div>
