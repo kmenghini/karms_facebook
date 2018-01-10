e03c6b69bfd62b2e87f750d1cb25b6da6494c2c6
Albert Chang <albertchanged@gmail.com>
2018-01-09 23:59:07 -0800

    Import axios, implement CreatePost, Post-related server routes
    All team members should do 'npm install' again after pulling.
    Created server routes for getting all posts and making a new post.
    Refactored Post and PostList to render input text

--- server/index.js
+++ server/index.js
@@ -7,9 +7,20 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 let port = 3000;
 
-app.get('/:username/posts/:otherusername', function(req, res) {
-  res.json(`hi ${req.params.username}, here are ${req.params.otherusername}'s posts`);
+// Get all posts
+app.get('/:username/posts', function(req, res) {
+  res.json(`hi from GET all posts, ${req.params.username}, here are all the posts`);
 });
+
+// Get posts by a certain user
+app.get('/:username/posts/:otherusername', function(req, res) {
+  res.json(`hi from GET user posts, ${req.params.username}, here are ${req.params.otherusername}'s posts`);
+});
+
+// Add new post to database
+app.post('/:username/posts', function(req, res) {
+  res.send(`hi from POST new post, ${req.params.username}, I made a post with this text "${req.body.text}"`);
+})
 
 app.listen(process.env.PORT || port, function() {
   console.log(`listening on port ${port}`);
