2bfe92233bebe8865c3b1adebe1a72bfa028bbd8
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-01-10 10:22:46 -0800

    added react-route-dom dependency

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,8 +5,12 @@ import PostList from './components/PostList.jsx';
 import Search from './components/Search.jsx';
 import Profile from './components/Profile.jsx';
 import Header from './components/Header.jsx';
+<<<<<<< HEAD
 import SignIn from './components/SignIn.jsx';
 import { Router, Route, Switch, Link } from 'react-router-dom';
+=======
+import { Router, Route, Switch, Link } from 'react-router-dom'
+>>>>>>> added react-route-dom dependency
 
 class App extends React.Component {
   constructor(props) {
