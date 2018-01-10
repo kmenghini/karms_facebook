360019aef4ff87622e90ab1922d81712851e0945
ShubhraJain <00sankalp@gmail.com>
2018-01-09 20:26:43 -0800

    Added Header component and WIP on Search component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Search from './Search.jsx';
+import { Icon } from 'semantic-ui-react';
+
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="header"> 
+        <Search />
+      </div>
+    );
+  }
+}
+
+export default Header;
+
+        // <Icon icon="facebook square" className="fb-icon" />     
