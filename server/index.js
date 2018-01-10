3a4c748ae4727306680ee7321c2d4e985baa33b5
Albert Chang <albertchanged@gmail.com>
2018-01-10 15:52:01 -0800

    Retry database and server functions for creating posts

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 let app = express();
 const bodyParser = require('body-parser');
+const db = require('../database-posgtres/index.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
@@ -18,8 +19,17 @@ app.get('/:username/posts/:otherusername', function(req, res) {
 });
 
 // Add new post to database
-app.post('/:username/posts', function(req, res) {
-  res.send(`hi from POST new post, ${req.params.username}, I made a post with this text "${req.body.text}"`);
+app.post('/:username/posts', function(req, res) {		  app.post('/:username/posts', function(req, res) {
+  console.log(req.params.username);		 +  res.send(`hi from POST new post, ${req.params.username}, I made a post with this text "${req.body.text}"`);
+  console.log(req.body.text);		
+  db.createPost(req.params.username, req.body.text, (err, data) => {		
+    if (err) {		
+      res.sendStatus(404);		
+    } else {		
+      res.status(200).json(data);		
+    }		
+  })		
+  // res.send(`hi from POST new post, ${req.params.username}, I made a post with this text "${req.body.text}"`);		
 })
 
 app.listen(process.env.PORT || port, function() {
