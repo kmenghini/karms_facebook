201e67625d12fad1c3593fb637c9465c948401fb
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-01-10 19:45:19 -0800

    added imports to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,17 +5,8 @@ import PostList from './components/PostList.jsx';
 import Search from './components/Search.jsx';
 import Profile from './components/Profile.jsx';
 import Header from './components/Header.jsx';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import SignIn from './components/SignIn.jsx';
 import { Router, Route, Switch, Link } from 'react-router-dom';
-=======
-import { Router, Route, Switch, Link } from 'react-router-dom'
->>>>>>> added react-route-dom dependency
-=======
-import SignIn from './components/SignIn.jsx';
-import { Router, Route, Switch, Link } from 'react-router-dom';
->>>>>>> added NewUser component
 
 class App extends React.Component {
   constructor(props) {
