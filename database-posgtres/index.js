a5cfec4c16e204ac66348a7d3f05e94128fd05cf
Albert Chang <albertchanged@gmail.com>
2018-01-10 22:31:04 -0800

    Set up new config var for Database

--- database-posgtres/index.js
+++ database-posgtres/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 console.log('Initializing client');
 console.log(process.env.DATABASE_URL);
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.PG_DATABASE_URL,
   ssl: true
 });
 
