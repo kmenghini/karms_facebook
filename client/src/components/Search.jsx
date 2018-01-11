56065d82bc0f976acbcedcaa115ce485a3da3562
ShubhraJain <00sankalp@gmail.com>
2018-01-11 09:53:58 -0800

    changed search bar from semantic ui to regular html

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -11,42 +11,87 @@ class Search extends React.Component {
     this.state = {
       gettingUser: false,
       // users: []
-      users: [{key: 1, text: 'Shubhra'}, {key: 2, text: 'Albert'}, {key: 3, text: 'Ryan'}, {key: 4, text: 'Kaitlyn'}, {key: 5, text: 'Matt'}]
+      users: [{key: 1, text: 'Shubhra'}, 
+              {key: 2, text: 'Albert'}, 
+              {key: 3, text: 'Ryan'}, 
+              {key: 4, text: 'Kaitlyn'}, 
+              {key: 5, text: 'Matt'}],
+      selectedUser: ''
     }
   }
 
-  getAllUsers() {
-    this.setState ({
-      users: [{key: 1, text: 'Shubhra'}, {key: 2, text: 'Albert'}, {key: 3, text: 'Ryan'}, {key: 4, text: 'Kaitlyn'}, {key: 5, text: 'Matt'}]
-    }); 
-    // let userName = 'ShubhraJain'
-    //   axios.get(`/${userName}/user`)
-    //   .then((res) => {
-    //     console.log(res);
-    //     this.setState ({
-    //       users: res
-    //     })      
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   })
+  handleSearch(event) {
+    event.preventDefault();
+    console.log('form submit');
+    let user = this.refs.searchUser.value;
+    let userName = 'ShubhraJain'
+    if (user) {
+      axios.get(`/${userName}/user`)
+      .then((res) => {
+        console.log(res);
+        this.setState ({
+          users: res
+        })      
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+
+  handleKeyPress(event) {
+    if (event.key !== 'Enter') {
+      // call to db to show results related to search string
+    }
   }
 
   render() {
     return (
-      <div className="search-bar">
-        <Dropdown 
+      <form className="search-bar" onSubmit={this.handleSearch.bind(this)}>
+        <input 
           className="search-input" 
-          // onClick={this.getAllUsers.bind(this)} 
-          placeholder='Search' 
-          fluid search selection 
-          options={this.state.users} />
-      </div>
+          type="text" 
+          placeholder="Search" 
+          ref="searchUser" 
+          // onKeyPress={this.handleKeyPress.bind(this)}
+        />
+        <button className="search-btn">Find</button>
+      </form>
     )
   }
 }
 
 export default Search;
+  // getAllUsers() {
+  //   this.setState ({
+  //     // users: [{key: 1, text: 'Shubhra'}, 
+  //     //         {key: 2, text: 'Albert'}, 
+  //     //         {key: 3, text: 'Ryan'}, 
+  //     //         {key: 4, text: 'Kaitlyn'}, 
+  //     //         {key: 5, text: 'Matt'}]
+  //     // selectedUser: 
+  //   }); 
+
+  // selectUser(e, { data }) {
+  //   console.log('event: ', e);
+  //   console.log('data:', data);
+  //   // this.setState({
+  //   //   selectedUser: data.value
+  //   // })
+  // }
+        
+        // <Dropdown 
+          // className="search-input" 
+          // placeholder='Search' 
+          // fluid
+          // search
+          // selection 
+          // options={this.state.users}
+          // ref="searchUser"
+          // onChange={this.selectUser.bind(this)} 
+          // onSearchChange={this.handleSearchChange}
+          // value={value} 
+          // />
 
 // handle click on search icon
 // post request to /:username/search/:otherUsername
