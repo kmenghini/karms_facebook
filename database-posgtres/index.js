b737269495eb020665674b0c3f041e6397e887af
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-01-12 15:07:32 -0800

    complete query to get friends list

--- database-posgtres/index.js
+++ database-posgtres/index.js
@@ -225,22 +225,6 @@ module.exports = {
       }
     });
 
-  },
-  getUserPosts: (username, callback) => {
-    // var queryStr = `SELECT posts.*, users.* FROM posts INNER JOIN users ON posts.user_id = users.id WHERE users.id = (SELECT users.id FROM users WHERE users.username = ${username})`;
-    // var queryStr = `SELECT posts.*, users.first_name, users.last_name FROM posts INNER JOIN users ON users.id = posts.user_id ORDER BY id DESC`;
-    var query = {
-      text: 'SELECT posts.*, users.* FROM posts INNER JOIN users ON posts.user_id = users.id WHERE users.id = (SELECT users.id FROM users WHERE users.username = $1)',
-      values: [username]
-    };
-    client.query(query, (err, res) => {
-      if (err) {
-        console.log('error...', err);
-        callback(err, null);
-      } else {
-        callback(null, res.rows);
-      }
-    });
   }
 }
 
