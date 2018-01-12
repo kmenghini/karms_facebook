5382344ab3a1531f0e0f22649e04650ac94dec2c
ShubhraJain <00sankalp@gmail.com>
2018-01-12 12:14:47 -0800

    Took out some extra lines

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -28,18 +28,6 @@ class Search extends React.Component {
     this.refs.searchUser.value = '';
   }
 
-  // TODO if enough time
-  // handleKeyPress(event) {
-  //   if (event.key !== 'Enter') {
-  //     console.log('hi');
-  //     axios.get(`/${userName}/user`)
-      
-      
-  //     // call to db to show results related to search string
-  //   }
-  // }
-
-
   render() {
     return (
       <form className="search-bar" onSubmit={this.handleSearch.bind(this)}>
@@ -48,7 +36,6 @@ class Search extends React.Component {
           type="text" 
           placeholder="Search" 
           ref="searchUser" 
-          // onKeyPress={this.handleKeyPress.bind(this)}
         />
         <button className="search-btn">Find</button>
       </form>
@@ -57,49 +44,3 @@ class Search extends React.Component {
 }
 
 export default Search;
-
-  // TODO if enough time
-  // handleKeyPress(event) {
-  //   if (event.key !== 'Enter') {
-  //     console.log('hi');
-  //     axios.get(`/${userName}/user`)
-      
-      
-  //     // call to db to show results related to search string
-  //   }
-  // }
-
-  // getAllUsers() {
-  //   this.setState ({
-  //     // users: [{key: 1, text: 'Shubhra'}, 
-  //     //         {key: 2, text: 'Albert'}, 
-  //     //         {key: 3, text: 'Ryan'}, 
-  //     //         {key: 4, text: 'Kaitlyn'}, 
-  //     //         {key: 5, text: 'Matt'}]
-  //     // selectedUser: 
-  //   }); 
-
-  // selectUser(e, { data }) {
-  //   console.log('event: ', e);
-  //   console.log('data:', data);
-  //   // this.setState({
-  //   //   selectedUser: data.value
-  //   // })
-  // }
-        
-        // <Dropdown 
-          // className="search-input" 
-          // placeholder='Search' 
-          // fluid
-          // search
-          // selection 
-          // options={this.state.users}
-          // ref="searchUser"
-          // onChange={this.selectUser.bind(this)} 
-          // onSearchChange={this.handleSearchChange}
-          // value={value} 
-          // />
-
-// handle click on search icon
-// post request to /:username/search/:otherUsername
-// query db: select * from user where username === otherUsername
