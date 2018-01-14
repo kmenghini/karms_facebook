fe4aa1cb395f8b58493fb2993323ac22b707c29f
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-01-13 20:47:23 -0800

    add semantic icon to errors

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
-import {Input, Button, Card, Image, Form, Field} from 'semantic-ui-react';
+import {Input, Button, Card, Image, Form, Field, Icon} from 'semantic-ui-react';
 import { Link, Redirect } from 'react-router-dom';
 
 class NewUser extends React.Component {
@@ -57,12 +57,12 @@ class NewUser extends React.Component {
     return(
       <div className="newUser">
         {this.props.usernameError ?
-        <h3><font color="red"> Username '{this.props.username}' doesn't match any account.</font></h3> : null}
+        <h3><font color="red"><Icon name="warning circle"/>Username '{this.props.username}' doesn't match any account.</font></h3> : null}
         <h4 id="new-account-title">Create a New Account</h4>
         <Card className="new-user-card">
           <Image className="ui tiny images" src="/images/profile_default.jpg"/>
           <Form className="input-form" onSubmit={this.handleSubmit.bind(this)}>
-          {this.state.invalidInput ? <h5 className="undefined-user-error">All fields are required. Please enter your info and try again.</h5> : null}
+          {this.state.invalidInput ? <h5 className="undefined-user-error"><Icon name="warning circle"/>All fields are required. Please enter your info and try again.</h5> : null}
             <Input className="newUserInput" name="username" type="text" onChange={this.handleInputChange.bind(this)} placeholder="Username"/>
             <Input className="newUserInput" name="firstName" type="text" onChange={this.handleInputChange.bind(this)} placeholder="First name"/>
             <Input className="newUserInput" name="lastName" type="text" onChange={this.handleInputChange.bind(this)} placeholder="Last name"/>
