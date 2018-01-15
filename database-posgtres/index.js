9e9936b45b2bd0d97f983b4b4d0b22a3409d70c7
Ryan Ngo <rayango92@gmail.com>
2018-01-14 23:18:13 -0800

    fix connection string

--- database-posgtres/index.js
+++ database-posgtres/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 console.log('Initializing client');
 console.log('This is the database url', process.env.DATABASE_URL);
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/fb_database'
+  connectionString: process.env.DATABASE_URL || 'postgres://rngo@localhost:5432/fb_database'
 });
 
 client.connect();
